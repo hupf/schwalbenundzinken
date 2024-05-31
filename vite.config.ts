b054c18882b8b90df36cb34837a90302cbd86578
Mathis Hofer <mathis@fsfe.org>
2024-05-31 13:42:13 +0200

    Configure PWA using vite-plugin-pwa

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Schwalben und Zinken",
+        short_name: "Schwalben & Zinken",
+        description: "Dovetail joint calculator and visualizer",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
